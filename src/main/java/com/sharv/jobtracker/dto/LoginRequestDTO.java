9ea22fea7a0c8768e06c3dc823706084b1d9ee84
sharv2884 <sharvaniar04@gmail.com>
2026-06-21 21:15:52 +0530

    Multi-user JWT Authentication done

---
+++ src/main/java/com/sharv/jobtracker/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.sharv.jobtracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
